5021514348a857555b77b80ede97bbf328b5f023
Andrey Murzin <saika@bk.ru>
2020-11-03 12:30:39 +0300

    ticket

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        boolean registered = true;
+        int ticketcost = 1885;
+        int bonus = ticketcost/20;
+        System.out.println(bonus);
+    }
+}
